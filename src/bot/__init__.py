c65c1628410364d7760c9767c7fe241855dff0c4
FoobarProtocol <librehash@gmail.com>
2025-03-06 06:51:00 -0500

    Fix typo in bot module __all__ list
    - Correct syntax error in __all__ list for slash command imports
    - Ensure proper module export of slash command functions

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -3,4 +3,4 @@ from .commands import register_commands
 from .events import register_events
 from .slash_commands import register_slash_commands, setup_slash_commands
 
-__all__ = ['create_bot', 'run_bot', 'register_commands', 'register_events', 'setup_slash_commands']
+__all__ = ['create_bot', 'run_bot', 'register_commands', 'register_events', 'register_slash_commands' 'setup_slash_commands']
